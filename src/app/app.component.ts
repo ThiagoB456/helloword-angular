e9456274c6d8639396e564496088e4e88e364848
Renan <renansilvademelo1995@gmail.com>
2022-08-15 09:01:55 -0300

    Aula 15/08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloMundo';
+  title = 'Renan e Thiaga';
+  logo = 'favicon.ico';
+  exibe = true;
+
+  exibirNome(){
+    return 'Thiaga';
+   }
+
+   exibirAlerta(){
+    alert("Hoje tem Prova ;-;");
+   }
+
+   Esconder(){
+    this.exibe = false;
+    if(this.exibe = false){
+     return "";
+    }else{
+      return "Texto exibido"
+    }
+   }
 }
+
